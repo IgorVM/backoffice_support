ddaf0127f4f9477d050285b96186b84fbc554bd5
Igor Morozov <igor@avtobus66.ru>
2014-12-15 13:31:27 +0500

    BackSupport Project was added

---
+++ BackOfficeSupport/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "О нас";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Где нас найти";
+
+            return View();
+        }
+
+        public ActionResult LeaveMessage()
+        {
+            ViewBag.Message = "Оставить заявку о неисправности или пожелание";
+
+            return View();
+        }
+    }
+}
