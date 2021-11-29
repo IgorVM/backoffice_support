e057536d207ec040e349da7cd5238eebae4c7128
Igor Morozov <ivm.ekb@gmail.com>
2021-11-30 01:11:12 +0500

    Test4

--- BackOfficeSupport/Controllers/HomeController.cs
+++ BackOfficeSupport/Controllers/HomeController.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
-
 namespace MvcApplication1.Controllers
 {
     public class HomeController : Controller
